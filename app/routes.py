f23c12f2b04f4f8cfe0db570cc9325bb1d8090ee
Qinghui Ge <qinghuige@Kashyyyk.local>
2019-09-20 13:58:59 -0400

    hello world

---
+++ app/routes.py
@@ -0,0 +1,32 @@
+from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/blog')
+def blog():
+    user = {'username': 'root'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('blog.html', title='Home', user=user, posts=posts)
