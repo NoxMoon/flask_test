ac07414bfb023875d0a79fc7a699ad2a8fd593a0
Qinghui Ge <qinghuige@Kashyyyk.local>
2019-12-16 17:49:57 -0500

    js

--- app/claim_telematics.py
+++ app/claim_telematics.py
@@ -71,11 +71,15 @@ def plot_multiple_trip(trip_list,
         window_xlims = ((left - time_offset).total_seconds() - window_width * XLIM_DISPLAY_BUFFER,
                     (right - time_offset).total_seconds() + window_width * XLIM_DISPLAY_BUFFER)
 
-        plt.figure(figsize=kwargs.get('figsize', (12, 12)))
+        #plt.figure(figsize=kwargs.get('figsize', (12, 12)))
         if trip.has_gps and 'latitude' in trip.gps.columns and 'longitude' in trip.gps.columns:
             m = folium.Map(location=[trip.gps.latitude.loc[left:right].median(),
                                         trip.gps.longitude.loc[left:right].median()],
-                            tiles="OpenStreetMap", zoom_start=kwargs.get('zoom_start', 13))
+                            tiles="OpenStreetMap", zoom_start=kwargs.get('zoom_start', 13),
+                            width='75%',
+                            height='75%')
+            #print(trip.gps.latitude.loc[left:right].median(), trip.gps.longitude.loc[left:right].median())
+            
             folium.PolyLine(list(zip(trip.gps.latitude.loc[left:right],
                                         trip.gps.longitude.loc[left:right])), color='black').add_to(m)
             if trip.trip_end < right:
@@ -109,54 +113,6 @@ def plot_multiple_trip(trip_list,
 
 
 
-def get_data_file_name(id_list, trip_id = True):
-    if trip_id:
-        data_file_name_list = []
-        for trip_id in id_list:
-            data_file_name = pd.read_sql_query(
-             ''' 
-            select data_file_name from telematics_public.trips where id = '%s'
-            '''%trip_id,connection
-
-
-                )
-            data_file_name_list.append(data_file_name.iloc[0,0])
-        return(data_file_name_list)
-    else:
-        data_file_name_list = []
-        for user_id in id_list:
-            data_file_name = pd.read_sql_query(
-                ''' 
-            WITH temp1(telematics_user_id, trip_count, start_time, end_time, duration) AS
-  (SELECT t1.telematics_user_id,
-          count(t1.data_file_name),
-          min(t1.start) AS start_time,
-          max(t1.end) AS end_time,
-          max(t1.end) - min(t1.start) AS duration
-   FROM telematics_public.trips t1
-   WHERE t1.gps_coordinates_disabled != 'true'
-     AND t1.start > 2015
-     AND t1.end < CURRENT_DATE
-   GROUP BY t1.telematics_user_id
-   HAVING DATEDIFF(DAY, start_time, end_time)>=21 and telematics_user_id = '%s')
-SELECT t1.telematics_user_id,
-       t1.id,
-       t1.start,
-       t1.end,
-       t1.data_file_name
-FROM telematics_public.trips t1
-INNER JOIN temp1 ON t1.telematics_user_id = temp1.telematics_user_id
-WHERE t1.end < temp1.end_time
-  AND DATEDIFF(DAY, t1.start, temp1.end_time)<=7
-  AND t1.distance != ''
-ORDER BY t1.end ASC
-            '''%user_id,connection
-
-
-                )
-            data_file_name_list.append(data_file_name.iloc[:,4])
-        return(data_file_name_list)
-
 import geopy
 def min_dist_to_location(trip, location):
     def f(x):
@@ -167,7 +123,25 @@ def min_dist_to_location(trip, location):
     return trip.gps[['latitude','longitude']].apply(f, axis=1).min()
 
 
-def get_trip_list(claim_number, start_date, end_date):
+from flask_table import Table, Col, LinkCol, ButtonCol
+class TripTable(Table):
+    trip = ButtonCol('get trip', endpoint='index')
+    trip_id = Col('trip_id')
+    start_time_local = Col('start_time_local')
+    end_time_local = Col('end_time_local')
+    distance = Col('distance')
+
+        
+    def save(self, filename):
+        f = open(filename, "w")
+        f.write(self.__html__())
+        f.close()
+
+
+def get_trip_list(claim_number):
+
+    #return pd.DataFrame()
+
     connection = db_connection('production_follower')
     connection.autocommit = True
     telematics_user_id = pd.read_sql_query(
@@ -198,6 +172,9 @@ def get_trip_list(claim_number, start_date, end_date):
     '''%in_condition
 
     trip_info = pd.read_sql_query(sql_query, connection)
+    
+    if len(trip_info)==0:
+        return None
 
     trip_info = trip_info.sort_values(by = 'start').reset_index(drop = True)
     trip_info.data_file_name[0]
@@ -206,25 +183,43 @@ def get_trip_list(claim_number, start_date, end_date):
     time_zone_offset = timedelta(hours=trip.meta['time_zone_offset'].components.hours-24)
     trip_info['start_time_local'] = trip_info['start'] + time_zone_offset
     trip_info['end_time_local'] = trip_info['end'] + time_zone_offset
+    
+    return trip_info
 
-    interested_trips = trip_info.loc[trip_info.start_time_local >= start_date].loc[trip_info.start_time_local <= end_date]
-    return interested_trips
+#     interested_trips = trip_info.loc[trip_info.start_time_local >= start_date].loc[trip_info.start_time_local <= end_date]
+
+#     d = interested_trips.to_dict(orient='record')
+#     table = TripTable(d)
+#     table.border = True
+#     table.html_attrs = {'cellpadding': 10}
+#     table.save('app/templates/table.html')
+#     return interested_trips
 
 
-def plot_trips(interested_trips, location=None):
+def plot_trips(interested_trips, claim_number='map', location=None):
+
+#    start_coords = (46.9540700, 142.7360300)
+#    folium_map = folium.Map(location=start_coords, zoom_start=14)
+#    return folium_map
+#    trip_list = []
+#    trip = get_trip("9dd089f1-93a8-4bd2-a15c-247a39788f8e.json.gz")
+#    trip_list.append(trip)
+#    plots = plot_multiple_trip(trip_list, accident_location=location)
+#    plots.save('app/templates/map.html')
+#    return
+
     trip_list = []
     interested_trips['min_dist'] = np.nan
     interested_trips['has_gps'] = False
-    #for i,data_file_name in enumerate(interested_trips.data_file_name):
-    for i, data_file_name in enumerate(["9dd089f1-93a8-4bd2-a15c-247a39788f8e.json.gz"]):
+    for i,data_file_name in enumerate(interested_trips.data_file_name):
         trip = get_trip(data_file_name)
         if trip is not None:
             if trip._has_gps:
                 interested_trips['has_gps'].iloc[i] = True
                 interested_trips['min_dist'].iloc[i] = min_dist_to_location(trip, location)
                 trip_list.append(trip)
-        break
-    print(trip_list)
+#    print(trip_list)
     plots = plot_multiple_trip(trip_list, accident_location=location)
-    print("done plotting")
-    return plots
+#    print("done plotting")
+    plots.save(f'app/templates/map_{claim_number}.html')
+    return
--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,117 @@
 from app import app
 from app.forms import ClaimForm
 from flask import render_template, flash, redirect, url_for, request
-from app.claim_telematics import get_trip_list, plot_trips
+from app.claim_telematics import TripTable, get_trip_list, plot_trips
+from flask_table import Table, Col, LinkCol, ButtonCol
+from collections import deque
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+
+
+    # Declare your table
+    # class ItemTable(Table):
+    #    name = Col('name')
+    #    description = LinkCol('description', endpoint='index')
+
+    # items = [dict(name='Name1', description='Description1'),
+    #         dict(name='Name2', description='Description2'),
+    #         dict(name='Name3', description='Description3')]
+
+    # table = ItemTable(items)
+    # return table.__html__()
+
     form = ClaimForm()
     if form.validate_on_submit():
         claim_number = form.claim_num.data
         start_date = form.start_date.data
         end_date = form.end_date.data
-        interested_trips = get_trip_list(claim_number, start_date, end_date)
-        #return redirect(url_for('index'))
-        #return render_template('result.html', title=str(claim_number), tables=[interested_trips.to_html(classes='data', header="true")])
-        #return claim_result(claim_number, interested_trips)
-        #return redirect(url_for('.claim_result', claim_number=str(claim_number), trips=interested_trips))
-        trip_map = plot_trips(interested_trips)
-        trip_map.save('templates/map.html')
-        return redirect(url_for('.claim_result', claim_number=claim_number, trips=interested_trips.to_html(classes='data', header="true")))
+        #interested_trips = get_trip_list(claim_number, start_date, end_date)
+        
+        #if not interested_trips:
+        #    return "no trip found"
+
+        #plot_trips(interested_trips, claim_number)
+
+        #return redirect(url_for('.claim_result', claim_number=claim_number))
+        
+        return redirect(url_for('result', claim_number=claim_number, start_date=start_date, end_date=end_date))
     return render_template('index.html', title='Claim Telematics', form=form)
 
-@app.route('/result/<claim_number>')
-def claim_result(claim_number):
-    #return render_template('result.html', title=str(claim_number), tables=[trips.to_html(classes='data', header="true")])
-    return render_template('result.html', title=claim_number, tables=[request.args.get('trips')])
+#@app.route('/result/<claim_number>')
+#def claim_result(claim_number):
+#    return render_template('result.html', title=claim_number)
+#    #return render_template('result.html', title=claim_number, tables=[request.args.get('trips')])
+    
+    
+results = dict()
+    
+@app.route('/result')
+def result():
+    claim_number = request.args.get('claim_number')
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    
+    if not claim_number in results:
+        trips = get_trip_list(claim_number)
+        results[claim_number] = trips
+        
+    if not trip:
+        return "No trip found for user"
+    
+    return render_template('result.html', claim_number=claim_number, start_date=start_date, end_date=end_date)
+    #return redirect(url_for('trip_table', claim_number=claim_number, start_date=start_date, end_date=end_date))
+    #return render_template('result.html', title=claim_number)
+    #return render_template('result.html', title=claim_number, tables=[request.args.get('trips')])
+ 
+@app.route('/trip_table')
+def trip_table():
+    claim_number = request.args.get('claim_number')
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    if not claim_number in results:
+        trips = get_trip_list(claim_number)
+        results[claim_number] = trips
+        
+    trips = results[claim_number]
+    interested_trips = trips.loc[trips.start_time_local >= start_date].loc[trips.start_time_local <= end_date]
+    if len(trip)==0:
+        return "no trip found in time frame"
 
+    d = interested_trips.to_dict(orient='record')
+    table = TripTable(d)
+    table.border = True
+    table.html_attrs = {'cellpadding': 2}
+    return table.__html__()
+
+@app.route('/trip_map')
+def trip_map():
+    claim_number = request.args.get('claim_number')
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    trip = request.args.get('trip')
+    location = (request.args.get('lat'), request.args.get('long'))
+    
+    if claim number:
+        if not claim_number in results:
+            trips = get_trip_list(claim_number)
+            results[claim_number] = trips
+        
+        trips = results[claim_number]
+        interested_trips = trips.loc[trips.start_time_local >= start_date].loc[trips.start_time_local <= end_date]
+        trip_files = list(interested_trips.data_file_name)
+        
+    elif trip:
+        trip_files = list(trip)
+    
+    filename = 'maps/'+hash(tuple(trip_files))
+    if not os.path.isfile(filename):
+        plot_trips(trip_files, location=None, filename)
+        
+    file = open(filename, "r")
+    return file.readlines()
+    
 
 @app.route('/blog')
 def blog():
